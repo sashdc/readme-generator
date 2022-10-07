7f30c230dd94361082b1ec70f1063f4daf670289
saharsh <saharshdc@gmail.com>
2022-10-07 14:53:56 -0400

    fixed table of contents

--- index.js
+++ index.js
@@ -1,16 +1,7 @@
-// GIVEN a command-line application that accepts user input
-// WHEN I am prompted for information about my application repository
-// THEN a high-quality, professional README.md is generated with the title of my project and sections entitled Description, Table of Contents, Installation, Usage, License, Contributing, Tests, and Questions
-// WHEN I enter my project title
-// THEN this is displayed as the title of the README
-// WHEN I enter a description, installation instructions, usage information, contribution guidelines, and test instructions
-// THEN this information is added to the sections of the README entitled Description, Installation, Usage, Contributing, and Tests
+
 // WHEN I choose a license for my application from a list of options
 // THEN a badge for that license is added near the top of the README and a notice is added to the section of the README entitled License that explains which license the application is covered under
-// WHEN I enter my GitHub username
-// THEN this is added to the section of the README entitled Questions, with a link to my GitHub profile
-// WHEN I enter my email address
-// THEN this is added to the section of the README entitled Questions, with instructions on how to reach me with additional questions
+
 // WHEN I click on the links in the Table of Contents
 // THEN I am taken to the corresponding section of the README
 
@@ -18,10 +9,45 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 
+function renderBadge(license){
+  if (license==='MIT'){
+    return '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)'
+  }
+  if (license==='Apache 2.0 License'){
+    return '[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)'
+  }
+  if (license==='Boost Software License 1.0'){
+    return '[![License](https://img.shields.io/badge/License-Boost_1.0-lightblue.svg)](https://www.boost.org/LICENSE_1_0.txt)'
+  }
+  if (license==='None'){
+    return ''
+  }
+}
+
+function renderLicenseLink(license) {
+  if (license==='None'){
+    return''
+  }
+  else {
+    return '- [License](#license)'}
+}
+
+function renderLicenseSection(license) {
+  if (license==='None'){
+    return ''
+  }
+  else {
+    return `## License
+    This project was created using ${license} license`
+  }
+}
+
+
 // create readme content
-const generateReadme = ({project, description, installation, contribution, test, license, github, email }) =>
+const generateReadme = ({project, description, installation, contribution, test, usage, license, github, email }) =>
 `# ${project}
 
+${renderBadge(license)}
 ## Description
 
 ${description}
@@ -30,8 +56,10 @@ ${description}
 
 - [Installation](#installation)
 - [Usage](#usage)
-- [Credits](#credits)
-- [License](#license)
+- [How To Contribute](#how-to-contribute)
+${renderLicenseLink(license)}
+- [Tests](#tests)
+- [Questions](#questions)
 
 ## Installation
 
@@ -45,22 +73,16 @@ ${usage}
 
 ${contribution}
 
-## License
-
-The last section of a high-quality README file is the license. This lets other developers know what they can and cannot do with your project. If you need help choosing a license, refer to [https://choosealicense.com/](https://choosealicense.com/).
-
----
-
-🏆 The previous sections are the bare minimum, and your project will ultimately determine the content of this document. You might also want to consider adding the following sections.
-
-## Badges
-
-![badmath](https://img.shields.io/github/languages/top/lernantino/badmath)
-
+${renderLicenseSection(license)}
 
 ## Tests
 
-${test}`
+${test}
+
+## Questions
+
+If you have any further questions, please don't hesitate to reach out to me via [email](${email}), or on GitHub through [my GitHub profile](http://www.github.com/${github}).
+`
 
 
 // TODO: Create an array of questions for user input
@@ -83,6 +105,11 @@ ${test}`
       },
       {
         type: 'input',
+        message: 'Any usage guidelines?',
+        name: 'usage',
+      },
+      {
+        type: 'input',
         message: 'Any contribution guidelines?',
         name: 'contribution',
       },
@@ -90,18 +117,18 @@ ${test}`
       type: 'input',
       message: 'Any test instructions?',
       name: 'test',
-    }
-    // {
-    //     type: 'list',
-    //     message: 'Please select and applicable license.',
-    //     choices: ['alligator', 'crocodile'],
-    //     name: 'license',
-    //   }
+    },
+    {
+        type: 'list',
+        message: 'Please select an applicable license.',
+        choices: ['MIT', 'Apache 2.0 License', 'Boost Software License 1.0','None'],
+        name: 'license',
+      },
       {
         type: 'input',
         message: 'Please enter your GitHub username.',
         name: 'github',
-      }
+      },
       {
         type: 'input',
         message: 'Please enter your email address.',
@@ -113,7 +140,7 @@ ${test}`
     const readmeContent = generateReadme(response);
 
     fs.writeFile('readme.md', readmeContent, (err) =>
-      err ? console.log(err) : console.log('Successfully created index.html!')
+      err ? console.log(err) : console.log('Successfully created ReadMe!')
     );
   });
 
